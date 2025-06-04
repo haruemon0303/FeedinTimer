f99035461613d4d8ff5648fcf294e0b94e8d04e8
Keisuke Obuchi <you@example.com>
2025-06-04 10:21:52 +0900

    Initial Commit

---
+++ FeedingTimerApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FeedingTimerApp2
+//
+//  Created by Obuchi Keisuke on 2025/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FeedingTimerApp2/FeedingTimerApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  FeedingTimerApp2App.swift
+//  FeedingTimerApp2
+//
+//  Created by Obuchi Keisuke on 2025/06/04.
+//
+
+import SwiftUI
+
+@main
+struct FeedingTimerApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
